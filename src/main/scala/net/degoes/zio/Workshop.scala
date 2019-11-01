746fc70d1aba83f8622976cc6f7b0db529aac8f4
tpetillot <tpetillot@gatling.io>
2019-11-01 13:21:12 +0100

    solved exercises

--- src/main/scala/net/degoes/zio/Workshop.scala
+++ src/main/scala/net/degoes/zio/Workshop.scala
@@ -1,6 +1,28 @@
 package net.degoes.zio
 
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+
 import zio._
+import zio.console.Console
+
+object Notes extends App {
+  def putStrLn2(line: String): ZIO[Console, Nothing, Unit] =
+    ZIO.accessM[Console](_.console.putStrLn(line))
+
+  val written: ZIO[Any, Nothing, Unit] =
+    putStrLn2("hell").provide(new Console {
+      val console = new Console.Service[Any] {
+        override def putStr(line: String): ZIO[Any, Nothing, Unit] = ???
+
+        override def putStrLn(line: String): ZIO[Any, Nothing, Unit] = ???
+
+        override val getStrLn: ZIO[Any, IOException, String] = ???
+      }
+    })
+
+  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = ???
+}
 
 object HelloWorld extends App {
   import zio.console._
@@ -11,7 +33,9 @@ object HelloWorld extends App {
     * Implement a simple "Hello World" program using the effect returned by `putStrLn`.
     */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ZIO.succeed(0)
+    putStrLn("Hello World!") *>
+      putStrLn("Goodbye World!") *>
+      ZIO.succeed(0)
 }
 
 object ErrorConversion extends App {
@@ -30,7 +54,9 @@ object ErrorConversion extends App {
     * Using `ZIO#orElse` or `ZIO#fold`, have the `run` function compose the
     * preceding `failed` effect into the effect that `run` returns.
     */
-  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = ???
+  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
+    failed.fold(_ => 1, _=> 0)
+    //failed map (_ => 0) orElse ZIO.succeed(1)
 }
 
 object PromptName extends App {
@@ -44,22 +70,43 @@ object PromptName extends App {
     * Implement a simple program that asks the user for their name (using
     * `getStrLn`), and then prints it out to the user (using `putStrLn`).
     */
-  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = ???
+  def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
+    (for {
+      _     <- putStrLn("Please enter your name: ")
+      name  <- getStrLn
+      _     <- putStrLn(s"Hello, $name!")
+    } yield ()).fold(_ => StdInputFailed, _ => 0)
 }
 
 object ZIOTypes {
   type ??? = Nothing
 
   /**
+    * ZIO[-R, +E, +A]
+    *
+    * A = The type with which the effect may succeed
+    *     (Corresponds to the `A` in `Future[A]`)
+    *
+    * E = The type with which the effect may failed
+    *     (Corresponds to the `Throwable`)
+    *
+    * R = The environment type required to run the effect
+    * R => Either(E, A]
+    *
+    * If we don't need R, we can use Any
+    * _           wan't to fail or succeed, we can use Nothing
+    */
+
+  /**
     * EXERCISE 4
     *
     * Provide definitions for the ZIO type aliases below.
     */
-  type Task[+A] = ???
-  type UIO[+A] = ???
-  type RIO[-R, +A] = ???
-  type IO[+E, +A] = ???
-  type URIO[-R, +A] = ???
+  type Task[+A] = ZIO[Any, Throwable, A]
+  type UIO[+A] = ZIO[Any, Nothing, A]
+  type RIO[-R, +A] = ZIO[R, Throwable, A]
+  type IO[+E, +A] = ZIO[Any, E, A]
+  type URIO[-R, +A] = ZIO[R, Nothing, A]
 }
 
 object NumberGuesser extends App {
@@ -77,7 +124,12 @@ object NumberGuesser extends App {
     * the number, feeding their response to `analyzeAnswer`, above.
     */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ???
+    (for {
+      random <- nextInt(10)
+      _      <- putStrLn("Guess a number from 0 to 10: ")
+      guess  <- getStrLn
+      _      <- analyzeAnswer(random, guess)
+    } yield ()).fold(_ => 1, _ => 0)
 }
 
 object AlarmApp extends App {
@@ -93,12 +145,29 @@ object AlarmApp extends App {
     */
   lazy val getAlarmDuration: ZIO[Console, IOException, Duration] = {
     def parseDuration(input: String): IO[NumberFormatException, Duration] =
-      ???
+      for {
+        double <- ZIO.effect(input.toDouble).refineToOrDie[NumberFormatException]
+      } yield (double * 1000.0).toInt.milliseconds
 
     def fallback(input: String): ZIO[Console, IOException, Duration] =
-      ???
+      parseDuration(input).catchAll(error =>
+        putStrLn(s"You enter $input, which is not a decimal: ${error.toString}") *>
+          getAlarmDuration
+      )
+      // parseDuration(input).foldM(
+      //   error => putStrLn(s"You enter $input, which is not a decimal: ${error.toString}") *>
+      //   getAlarmDuration,
+      //   duration => ZIO.succeed(duration)
+      // )
+      // parseDuration(input).tapError(error =>
+      //   putStrLn(s"You enter $input, which is not a decimal: ${error.toString}")
+      // ) orElse getAlarmDuration*/
 
-    ???
+    for {
+      _        <- putStrLn("Please enter the number of seconds to sleep (e.g. 1.5)")
+      input    <- getStrLn
+      duration <- fallback(input)
+    } yield duration
   }
 
   /**
@@ -109,13 +178,20 @@ object AlarmApp extends App {
     * alarm message.
     */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ???
+    (for {
+      _        <- putStrLn("Welcome to ZIO Alarm!")
+      duration <- getAlarmDuration
+      _        <- putStrLn(s"You will be awoken after $duration")
+      _        <- ZIO.sleep(duration)
+      _        <- putStrLn("Time to wake up!")
+    } yield ()).fold(_ => 1, _ => 0)
 }
 
 object Cat extends App {
   import zio.console._
   import zio.blocking._
   import java.io.IOException
+  import scala.io.Source
 
   /**
     * EXERCISE 8
@@ -123,7 +199,12 @@ object Cat extends App {
     * Implement a function to read a file on the blocking thread pool, storing
     * the result into a string.
     */
-  def readFile(file: String): ZIO[Blocking, IOException, String] = ???
+  def readFile(file: String): ZIO[Blocking, IOException, String] =
+    blocking {
+      ZIO.effect(Source.fromFile(file).mkString("\n"))
+        .refineToOrDie[IOException]
+    }
+
 
   /**
     * EXERCISE 9
@@ -132,7 +213,10 @@ object Cat extends App {
     * contents of the specified file to standard output.
     */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ???
+    (args match {
+        case Nil => putStrLn("You did not enter a file path to show!")
+        case _ :: file :: _ => readFile(file).flatMap(putStrLn(_))
+    }).fold(_ => 1, _=> 0)
 }
 
 object CatIncremental extends App {
@@ -147,12 +231,29 @@ object CatIncremental extends App {
     * the blocking thread pool.
     */
   final case class FileHandle private (private val is: InputStream) {
-    final def close: ZIO[Blocking, IOException, Unit] = ???
+    final def close: ZIO[Blocking, IOException, Unit] =
+      blocking {
+        ZIO.effect(is.close()).refineToOrDie[IOException]
+      }
 
-    final def read: ZIO[Blocking, IOException, Option[Chunk[Byte]]] = ???
+    final def read: ZIO[Blocking, IOException, Option[Chunk[Byte]]] =
+      blocking {
+        ZIO.effect {
+          val array = Array.ofDim[Byte](1024)
+          val read = is.read(array)
+
+          if (read == -1) None
+          else Some(Chunk.fromArray(array).take(read))
+        }.refineToOrDie[IOException]
+      }
   }
   object FileHandle {
-    final def open(file: String): ZIO[Blocking, IOException, FileHandle] = ???
+    final def open(file: String): ZIO[Blocking, IOException, FileHandle] =
+      blocking {
+        ZIO.effect {
+          new FileHandle(new FileInputStream(new File(file)))
+        }.refineToOrDie[IOException]
+      }
   }
 
   /**
@@ -163,9 +264,40 @@ object CatIncremental extends App {
     * interruption.
     */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ???
+    (args match {
+      case Nil => putStrLn("You did not enter a file path to show!")
+      case _ :: file :: _ =>
+        val managedFile = ZManaged.make(FileHandle.open(file))(_.close.orDie)
+        managedFile.use { handle =>
+          lazy val loop: ZIO[zio.console.Console with Blocking, IOException, Unit] = handle.read.flatMap {
+            case None => ZIO.succeed(())
+            case Some(chunk) => putStrLn(new String(chunk.toArray, StandardCharsets.UTF_8)) *> loop
+          }
+
+          loop
+        }
+    }).fold(_ => 1, _ => 0)
+
+    // (args match {
+    //   case Nil => putStrLn("You did not enter a file path to show!")
+    //   case _ :: file :: _ =>
+    //     FileHandle.open(file).bracket(_.close.orDie) { handle =>
+    //       lazy val loop: ZIO[zio.console.Console with Blocking, IOException, Unit] = handle.read.flatMap {
+    //         case None => ZIO.succeed(())
+    //         case Some(chunk) => putStrLn(new String(chunk.toArray, StandardCharsets.UTF_8)) *> loop
+    //       }
+    //
+    //       loop
+    //     }
+    // }).fold(_ => 1, _ => 0)
 }
 
+/**
+  * # note
+  *
+  * Ref can be seen as a concurrent safe version of var
+  * more: all methods of Ref are lazy
+  */
 object ComputePi extends App {
   import zio.random._
   import zio.console._
@@ -198,6 +330,15 @@ object ComputePi extends App {
   val randomPoint: ZIO[Random, Nothing, (Double, Double)] =
     nextDouble zip nextDouble
 
+  def sample(state: PiState, iterations: Int): ZIO[Random, Nothing, Unit] =
+    if (iterations <= 0) ZIO.unit // equivalent to ZIO.f(())
+    else (for {
+      random  <- randomPoint
+      (x, y)  = random
+      _       <- state.total.update(_ + 1)
+      _       <- if (insideCircle(x, y)) state.inside.update(_ + 1) else ZIO.unit
+    } yield ()) *> sample(state, iterations - 1)
+
   /**
     * EXERCISE 12
     *
@@ -205,13 +346,22 @@ object ComputePi extends App {
     * ongoing estimates continuously until the estimation is complete.
     */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ???
+    (for {
+      total   <- Ref.make(0L)
+      inside  <- Ref.make(0L)
+      state   =  PiState(inside, total)
+      worker  = sample(state, 100000)
+      _       <- ZIO.collectAllPar(List.fill(4)(worker)) // Lookup why collectAllPar is more efficient than collectAll
+      tuple   <- inside.get zip total.get
+      _       <- putStrLn(s"The value of pi is approximately: ${estimatePi(tuple._1, tuple._2)}")
+    } yield ()).fold(_ => 1, _=> 0)
 }
 
 object Hangman extends App {
   import zio.console._
   import zio.random._
   import java.io.IOException
+  import net.degoes.zio.Hangman.GuessResult.{ Correct, Incorrect, Won, Lost, Unchanged }
 
   /**
     * EXERCISE 13
@@ -219,7 +369,11 @@ object Hangman extends App {
     * Implement an effect that gets a single, lower-case character from
     * the user.
     */
-  lazy val getChoice: ZIO[Console, Nothing, Char] = ???
+  lazy val getChoice: ZIO[Console, Nothing, Char] =
+    getStrLn.orDie.map(_.trim.toLowerCase.toList).flatMap {
+      case char :: Nil if char.isLetter => ZIO.succeed(char)
+      case _ => putStrLn("You did not enter a single character!") *> getChoice
+    }
 
   /**
     * EXERCISE 14
@@ -227,14 +381,18 @@ object Hangman extends App {
     * Implement an effect that prompts the user for their name, and
     * returns it.
     */
-  lazy val getName: ZIO[Console, IOException, String] = ???
+  lazy val getName: ZIO[Console, IOException, String] =
+    putStrLn("Please enter your name: ") *> getStrLn
 
   /**
     * EXERCISE 15
     *
     * Implement an effect that chooses a random word from the dictionary.
     */
-  lazy val chooseWord: ZIO[Random, Nothing, String] = ???
+  lazy val chooseWord: ZIO[Random, Nothing, String] = {
+    val dictionary = Dictionary.Dictionary
+    nextInt(dictionary.length).map(dictionary(_))
+  }
 
   /**
     * EXERCISE 17
@@ -242,7 +400,21 @@ object Hangman extends App {
     * Implement the main game loop, which gets choices from the user until
     * the game is won or lost.
     */
-  def gameLoop(state0: State): ZIO[Console, IOException, Unit] = ???
+  def gameLoop(state0: State): ZIO[Console, IOException, Unit] =
+    (for {
+      char <- getChoice
+      state = state0.addChar(char)
+      State(name, _, word) = state
+      _ <- renderState(state)
+      loop <- guessResult(state0, state, char) match {
+        case Won => putStrLn(s"Congratulations, $name, you won!!") as false
+        case Lost => putStrLn(s"Sorry, $name, you lost. The word was $word.") as false
+        case Unchanged => putStr("Sorry, $name, you already guessed this letter.") as true
+        case Correct => putStr("Good job, $name, you guess correctly. Keep going!") as true
+        case Incorrect => putStr("Sorry, that was incorrect. Try again!") as true
+      }
+      _ <- if (loop) gameLoop(state) else ZIO.unit
+    } yield ()).refineToOrDie[IOException]
 
   def renderState(state: State): ZIO[Console, Nothing, Unit] = {
 
@@ -301,7 +473,13 @@ object Hangman extends App {
     * and the above helper functions.
     */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    ???
+    (for {
+      word <- chooseWord
+      name <- getName
+      state = State(name, Set(), word)
+      _ <- renderState(state)
+      _ <- gameLoop(state)
+    } yield ()).fold(_ => 1, _ => 0)
 }
 
 /**
